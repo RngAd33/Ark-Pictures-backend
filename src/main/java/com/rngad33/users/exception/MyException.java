7f56602332f97d52e40f638b540e98f1d9704f20
RngAd33 <yxbsyx@outlook.com>
2025-04-28 21:13:44 +0800

    init

---
+++ src/main/java/com/rngad33/users/exception/MyException.java
@@ -0,0 +1,55 @@
+package com.rngad33.users.exception;
+
+import com.rngad33.users.enums.ErrorCodeEnum;
+import lombok.Getter;
+
+/**
+ * 自定义异常类
+ */
+@Getter
+public class MyException extends RuntimeException {
+
+    /**
+     * 错误码
+     */
+    private final int code;
+
+    /**
+     * 报错信息
+     */
+    private final String msg;
+
+
+    /**
+     * 手动传参
+     *
+     * @param msg
+     * @param code
+     */
+    public MyException(String msg, int code) {
+        this.msg = msg;
+        this.code = code;
+    }
+
+    /**
+     * 自动传参
+     *
+     * @param errorCode
+     */
+    public MyException(ErrorCodeEnum errorCode) {
+        this.msg = errorCode.getMsg();
+        this.code = errorCode.getCode();
+    }
+
+    /**
+     * 半自动传参
+     *
+     * @param msg 自定义错误消息
+     * @param errorCode
+     */
+    public MyException(String msg, ErrorCodeEnum errorCode) {
+        this.msg = msg;
+        this.code = errorCode.getCode();
+    }
+
+}
---
+++ src/test/java/com/rngad33/users/ArkPicturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rngad33.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkPicturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
