6e95e0b070e0db4cc92b39e6dffe2fc444331a11
RngAd33 <yxbsyx@outlook.com>
2025-04-29 14:34:21 +0800

    图片模块开发

--- src/main/java/com/rngad33/users/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.rngad33.users.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.rngad33.users.model.User;
-
-/**
- * 用户 Mapper（继承 Mybatis-Plus）
- */
-public interface UserMapper extends BaseMapper<User> {}
---
+++ src/main/java/com/rngad33/web/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.rngad33.web.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rngad33.web.model.User;
+
+/**
+ * 用户 Mapper
+ */
+public interface UserMapper extends BaseMapper<User> {}
