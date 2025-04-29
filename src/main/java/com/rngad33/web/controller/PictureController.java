2d351d63332c5577d7d96d007ff7c787304c6531
RngAd33 <yxbsyx@outlook.com>
2025-04-29 14:34:38 +0800

    图片模块开发

---
+++ src/main/java/com/rngad33/web/controller/PictureController.java
@@ -0,0 +1,8 @@
+package com.rngad33.web.controller;
+
+/**
+ * 图片交互接口
+ */
+public class PictureController {
+
+}
---
+++ src/main/java/com/rngad33/web/service/impl/PictureServiceImpl.java
@@ -0,0 +1,12 @@
+package com.rngad33.web.service.impl;
+
+import com.rngad33.pictures.service.PictureService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 图片业务实现
+ */
+@Service
+public class PictureServiceImpl implements PictureService {
+
+}
