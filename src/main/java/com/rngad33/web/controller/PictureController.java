726516280edd4adaad96e543219a79c227650bcf
RngAd33 <yxbsyx@outlook.com>
2025-05-08 10:13:49 +0800

    图片模块开发

--- src/main/java/com/rngad33/web/controller/PictureController.java
+++ src/main/java/com/rngad33/web/controller/PictureController.java
@@ -1,8 +1,13 @@
 package com.rngad33.web.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * 图片交互接口
  */
+@RestController
+@RequestMapping("/pic")
 public class PictureController {
 
 }
