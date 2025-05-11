d947a718c4270a97bb9ce3fe052b00c714ce7bd0
RngAd33 <yxbsyx@outlook.com>
2025-05-11 16:15:12 +0800

    .gitignore

--- src/main/java/com/rngad33/web/utils/ThrowUtils.java
+++ src/main/java/com/rngad33/web/utils/ThrowUtils.java
@@ -11,7 +11,7 @@ public class ThrowUtils {
     /**
      * 条件成立则抛异常
      *
-     * @param condition        条件
+     * @param condition 条件
      * @param runtimeException 异常
      */
     public static void throwIf(boolean condition, RuntimeException runtimeException) {
