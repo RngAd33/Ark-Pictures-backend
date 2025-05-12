7e0e3f58690350e7b8fa3e37dda1335a6a39e4a2
RngAd33 <yxbsyx@outlook.com>
2025-05-12 12:42:26 +0800

    图片模块开发

--- src/main/java/com/rngad33/web/service/impl/PictureServiceImpl.java
+++ src/main/java/com/rngad33/web/service/impl/PictureServiceImpl.java
@@ -72,7 +72,7 @@ public class PictureServiceImpl extends ServiceImpl<PictureMapper, Picture> impl
             picture.setEditTime(new Date());
         }
         boolean result = this.saveOrUpdate(picture);   // 方法来自：MyBatis-Plus
-        ThrowUtils.throwIf(!result, ErrorCodeEnum.USER_LOSE_ACTION, "——！图片上传失败！——");
+        ThrowUtils.throwIf(!result, ErrorCodeEnum.USER_LOSE_ACTION, "——！上传失败！——");
         return PictureVO.objToVo(picture);
     }
 
