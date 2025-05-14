dc2d7c18c4258f798ed896b4359c26c5b0319d11
RngAd33 <yxbsyx@outlook.com>
2025-05-14 21:19:03 +0800

    图片模块开发

--- src/main/java/com/rngad33/web/model/entity/Picture.java
+++ src/main/java/com/rngad33/web/model/entity/Picture.java
@@ -93,6 +93,26 @@ public class Picture implements Serializable {
     private Date updateTime;
 
     /**
+     * 审核状态
+     */
+    private Integer reviewStatus;
+
+    /**
+     * 审核信息
+     */
+    private String reviewMessage;
+
+    /**
+     * 审核人id
+     */
+    private Long reviewId;
+
+    /**
+     * 审核时间
+     */
+    private Date reviewTime;
+
+    /**
      * 是否删除
      */
     @TableLogic
