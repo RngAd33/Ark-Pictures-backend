0c78e0d7ba5bb11d0fb129f5f13230e04017b457
RngAd33 <yxbsyx@outlook.com>
2025-05-14 21:36:05 +0800

    审核模块开发

--- src/main/java/com/rngad33/web/model/dto/picture/PictureReviewRequest.java
+++ src/main/java/com/rngad33/web/model/dto/picture/PictureReviewRequest.java
@@ -8,5 +8,21 @@ import lombok.Data;
 @Data
 public class PictureReviewRequest {
 
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 审核状态：0-待审核; 1-通过; 2-拒绝
+     */
+    private Integer reviewStatus;
+
+    /**
+     * 审核信息
+     */
+    private String reviewMessage;
+
+    private static final long serialVersionUID = 3191241716373120793L;
 
 }
