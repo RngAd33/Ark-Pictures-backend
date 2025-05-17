47529f91b44680b0d99793311790aa355c68e44a
RngAd33 <yxbsyx@outlook.com>
2025-05-17 22:33:20 +0800

    模板方法开发

--- src/main/java/com/rngad33/web/manager/upload/PictureUploadTemplateImpl.java
+++ src/main/java/com/rngad33/web/manager/upload/PictureUploadTemplateImpl.java
@@ -1,10 +1,19 @@
 package com.rngad33.web.manager.upload;
 
+import cn.hutool.core.io.FileUtil;
+import com.rngad33.web.model.enums.misc.ErrorCodeEnum;
+import com.rngad33.web.utils.ThrowUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 图片上传模板实现类
  */
+@Service
 public class PictureUploadTemplateImpl extends PictureUploadTemplate {
 
     /**
@@ -14,17 +23,29 @@ public class PictureUploadTemplateImpl extends PictureUploadTemplate {
      */
     @Override
     protected void validPicture(Object inputSource) {
-
+        // - 强制类型转换
+        MultipartFile multipartFile = (MultipartFile) inputSource;
+        // - 文件是否存在
+        ThrowUtils.throwIf(multipartFile == null, ErrorCodeEnum.PARAM_ERROR, "请先选择图片！");
+        // - 校验文件大小
+        ThrowUtils.throwIf(multipartFile.getSize() > 1024 * 1024 * 10,
+                ErrorCodeEnum.PARAM_ERROR, "图片大小不能超过10M！");
+        // - 校验文件后缀
+        String fileSuffix = FileUtil.getSuffix(multipartFile.getOriginalFilename());
+        final List<String> SUFFIX_ALLOW = Arrays.asList("png", "jpg", "jpeg", "gif", "webp");
+        ThrowUtils.throwIf(!SUFFIX_ALLOW.contains(fileSuffix), ErrorCodeEnum.PARAM_ERROR, "不支持的图片格式！");
     }
 
     /**
      * 获取输入源的原始文件名
      *
      * @param inputSource
+     * @return 原始文件名
      */
     @Override
     protected String getOriginFilename(Object inputSource) {
-        return "";
+        MultipartFile multipartFile = (MultipartFile) inputSource;
+        return multipartFile.getOriginalFilename();
     }
 
     /**
@@ -35,7 +56,8 @@ public class PictureUploadTemplateImpl extends PictureUploadTemplate {
      */
     @Override
     protected void processFile(Object inputSource, File file) throws Exception {
-
+        MultipartFile multipartFile = (MultipartFile) inputSource;
+        multipartFile.transferTo(file);
     }
 
 }
