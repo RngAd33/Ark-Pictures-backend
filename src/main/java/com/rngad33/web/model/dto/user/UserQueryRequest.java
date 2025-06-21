65534cbc21ca3d22863d4eee0ddb3042a6e4840b
RngAd33 <yxbsyx@outlook.com>
2025-06-21 23:27:23 +0800

    优化细节

--- src/main/java/com/rngad33/web/model/dto/user/UserQueryRequest.java
+++ src/main/java/com/rngad33/web/model/dto/user/UserQueryRequest.java
@@ -2,11 +2,13 @@ package com.rngad33.web.model.dto.user;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * 用户查询请求体
  */
 @Data
-public class UserQueryRequest {
+public class UserQueryRequest implements Serializable {
 
     private static final long serialVersionUID = 3191241716373120793L;
 
