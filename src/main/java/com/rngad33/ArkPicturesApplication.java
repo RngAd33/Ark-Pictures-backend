43a6f8ec8849a5b5e9a8944c90f21f600d30c042
RngAd33 <yxbsyx@outlook.com>
2025-06-27 21:43:18 +0800

    开发完成

--- src/main/java/com/rngad33/ArkPicturesApplication.java
+++ src/main/java/com/rngad33/ArkPicturesApplication.java
@@ -3,11 +3,13 @@ package com.rngad33;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * 项目启动入口
  */
 @SpringBootApplication
+@EnableAsync
 @MapperScan("com.rngad33.web.mapper")
 public class ArkPicturesApplication {
 
