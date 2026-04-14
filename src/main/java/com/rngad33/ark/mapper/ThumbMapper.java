d72426df14b075c2ee0053bf4f5a7946a05e78cc
RngAd33 <yxbsyx@outlook.com>
2026-04-14 16:43:35 +0800

    高并发点赞功能开发

--- src/main/java/com/rngad33/ark/mapper/ThumbMapper.java
+++ src/main/java/com/rngad33/ark/mapper/ThumbMapper.java
@@ -2,5 +2,10 @@ package com.rngad33.ark.mapper;
 
 import com.mybatisflex.core.BaseMapper;
 import com.rngad33.ark.model.entity.Thumb;
+import org.apache.ibatis.annotations.Param;
 
-public interface ThumbMapper extends BaseMapper<Thumb> {}
+import java.util.Map;
+
+public interface ThumbMapper extends BaseMapper<Thumb> {
+    void batchUpdateThumbCount(@Param("countMap") Map<Long, Long> countMap);
+}
