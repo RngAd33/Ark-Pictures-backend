810b2d5ad96d93d1c171f51defa5baa9be3730dc
RngAd33 <yxbsyx@outlook.com>
2026-05-09 15:21:10 +0800

    高并发点赞功能开发

--- src/main/java/com/rngad33/ark/utils/RedisKeyUtils.java
+++ src/main/java/com/rngad33/ark/utils/RedisKeyUtils.java
@@ -4,12 +4,21 @@ import com.rngad33.ark.constant.ThumbConstant;
 
 public class RedisKeyUtils {
 
+    /**
+     * 获取用户点赞记录 key
+     *
+     * @param userId
+     * @return
+     */
     public static String getUserThumbKey(Long userId) {
         return ThumbConstant.USER_THUMB_KEY_PREFIX + userId;
     }
 
     /**
-     * 获取 临时点赞记录 key
+     * 获取临时点赞记录 key
+     *
+     * @param time
+     * @return
      */
     public static String getTempThumbKey(String time) {
         return ThumbConstant.TEMP_THUMB_KEY_PREFIX.formatted(time);
